a407f52f43390aa725709e31d9cef89ea6f1d8c2
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-09-06 23:26:41 -0400

    simple 3rd person camera

---
+++ Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    float camRotX;
+    float camRotY;
+
+    public float sensitivity = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 mouseMovement = new Vector2(-Input.GetAxis("Mouse Y") * sensitivity, Input.GetAxis("Mouse X") * sensitivity);
+        camRotX -= mouseMovement.x;
+        camRotY += mouseMovement.y;
+        camRotX = Mathf.Clamp(camRotX, -85, 85);
+        gameObject.transform.localRotation = Quaternion.Euler(-camRotX,camRotY, 0);
+
+        //gameObject.transform.Rotate(0, mouseMovement.y, 0);
+    }
+}
