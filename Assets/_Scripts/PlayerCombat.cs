baf0d2e791304150ed1b6a820ac85f491bc04496
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-09-13 15:26:29 -0400

    animation setup

---
+++ Assets/_Scripts/PlayerCombat.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //us 0,1,2, etc. to differentiate between light or heavy attacks and general differing inputs.
+    public void PlayerAttack(int attackType, GameObject body)
+    {
+
+    }
+}
