580cc796240e22287ca4a7b7212194c26af54004
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2025-02-26 23:31:00 -0500

    mostly notes / plans for what I want to do.

--- Assets/_Scripts/Just Notes lol.cs
+++ Assets/_Scripts/Just Notes lol.cs
@@ -4,11 +4,20 @@ public class JustNoteslol
     //build up from?
     //1. attacking
     //2. killing
-    //either a resource on the enemy (like a stance break)
-    //or a resource the player has to be spent
-    //if player resource could lose some when hit, could be a shield? then spend the shield to possess a body, keeping the "shield"
+    //a resource the player has to be spent
+    //if player resource could lose some when hit, could be a shield?
 
     //REASON TO POSSESS
     //base player does low damage with a dodge, can complete level without possessing but will be hard/slow
     //enemies will not have many/any forms of survivability, supposessed to be expendable. Possessions will do much better damage (even the grunts).
+
+    //BASE PLAYER
+
+    //ENEMIES
+    //Basic: standard zombie type enemy
+    //Giant: a mini-boss / high priority possession. maybe throws a rock or something?
+    //Jumper: applies more pressure than basic enemy / a more lethal version of basic
+    //Flyer: a harrasser, meant to debuff player to make enemies more lethal.
+    //         has an "anchor" that needs to be found and attacked/possessed to deal with flyer.
+    //         minecraft creaking + creaking heart dynamic.
 }
