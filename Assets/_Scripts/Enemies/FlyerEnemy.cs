d3d93d6a617e8d8ed1af57d9bf3cb5b325ef7f3b
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2025-03-01 17:13:08 -0500

    .

--- Assets/_Scripts/Enemies/FlyerEnemy.cs
+++ Assets/_Scripts/Enemies/FlyerEnemy.cs
@@ -25,16 +25,6 @@ public class FlyerEnemy : Enemy
         rb.velocity = (player.transform.position - transform.position).normalized * 2;
         bodyTakeover.bodyModel.transform.forward = bodyTakeover.followTarget.forward;
 
-        /*
-         Some thoughts/Plans for movement:
-        1. patrols in a orbit (like a vulture)
-        2. once detected a player, becomes bug like? shadows of evil weird bug enemies. stops to shoot.
-            would be easier to not do that ^.
-        3. maybe attempt to go above player and shoot straight down at them, incentivize player movement. while also not getting accidentally killed.
-            ^would also match better with player possession mode if it just becomes a 1st person turret that shoots.
-            ^ a cooler version would be bombing run manuever.
-
-        */
     }
 
     public override void Combat()
